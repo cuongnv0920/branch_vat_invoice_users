5d2d0d1866807fe04ae5fe7b9bebf18332927a34
NGUYEN VAN CUONG <cuongnv12@BIDVBANK.bidv.com>
2023-05-30 16:28:07 +0700

    table invoice

--- src/features/Invoice/components/InvoiceList/index.jsx
+++ src/features/Invoice/components/InvoiceList/index.jsx
@@ -1,5 +1,7 @@
+import { CheckBox } from "@mui/icons-material";
 import {
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -10,42 +12,153 @@ import Paper from "@mui/material/Paper";
 InvoiceList.propTypes = {};
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 10, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 11, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 12, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    stt: 1,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 2,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 3,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 4,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 5,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 6,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 7,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 8,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 9,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
+  {
+    stt: 10,
+    serial: "1K23THA",
+    no: "3288816",
+    date: "30/05/2023",
+    seller:
+      "Trung tâm Kinh doanh VNPT thành phố Hồ Chí Minh - Chi nhánh Tổng công ty Dịch vụ Viễn Thông",
+    payment: "1,000,000",
+  },
 ];
 
 function InvoiceList(props) {
   return (
-    <div>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Check box</TableCell>
-              <TableCell>STT</TableCell>
-              <TableCell>Mẫu số</TableCell>
-              <TableCell>Ký hiệu</TableCell>
-              <TableCell>Số hóa đơn</TableCell>
-              <TableCell>Ngày hóa đơn</TableCell>
-              <TableCell>Đơn vị cung cấp</TableCell>
-              <TableCell>Trạng thái</TableCell>
-              <TableCell>Số tiền</TableCell>
-              <TableCell>File đính kèm</TableCell>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 680 }}>
+        <Table stickyHeader sx={{ minWidth: 650 }} className="invoiceTable">
+          <TableHead className="invoiceTable__head">
+            <TableRow className="invoiceTable__rowHead">
+              <TableCell className="invoiceTable__cellHead">
+                Check box
+              </TableCell>
+              <TableCell className="invoiceTable__cellHead">STT</TableCell>
+              <TableCell className="invoiceTable__cellHead">Ký hiệu</TableCell>
+              <TableCell className="invoiceTable__cellHead">
+                Số hóa đơn
+              </TableCell>
+              <TableCell className="invoiceTable__cellHead">
+                Ngày hóa đơn
+              </TableCell>
+              <TableCell className="invoiceTable__cellHead">
+                Đơn vị cung cấp
+              </TableCell>
+              <TableCell className="invoiceTable__cellHead">
+                Trạng thái
+              </TableCell>
+              <TableCell className="invoiceTable__cellHead">Số tiền</TableCell>
+              <TableCell className="invoiceTable__cellHead">
+                File đính kèm
+              </TableCell>
             </TableRow>
           </TableHead>
+          <TableBody className="invoiceTable__body">
+            {rows.map((row, _) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                className="invoiceTable__rowBody"
+              >
+                <TableCell>
+                  <CheckBox />
+                </TableCell>
+                <TableCell>{row.stt}</TableCell>
+                <TableCell>{row.serial}</TableCell>
+                <TableCell>{row.no}</TableCell>
+                <TableCell>{row.date}</TableCell>
+                <TableCell>{row.seller}</TableCell>
+                <TableCell>
+                  <button>Đã duyệt</button>
+                </TableCell>
+                <TableCell>{row.payment}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
-    </div>
+    </Paper>
   );
 }
 
